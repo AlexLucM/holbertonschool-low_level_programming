84b8da9b540e3e0702d24db0e72327a4b21ae52f
AlexLucM <ouattaradounya@gmail.com>
2025-10-23 12:35:41 +0000

    fixed betty

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -10,13 +10,13 @@ int main(void)
 {
 int n = '0';
 
-        while (n <= '9')
+	while (n <= '9')
 {
-        putchar(n);
-        n++;
+	putchar(n);
+	n++;
 }
 {
-        putchar('\n');
+	putchar('\n');
 }
-        return (0);
+	return (0);
 }
