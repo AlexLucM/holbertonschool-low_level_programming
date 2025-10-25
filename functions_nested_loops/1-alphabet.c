fd2d8f5cce495535cf9d072812ad532f468a04b4
AlexLucM <ouattaradounya@gmail.com>
2025-10-25 20:51:41 +0000

    added file 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_alphabet - prints alphabet in lowwer case, followed by a nex line
+*
+* Return: void
+*/
+void print_alphabet(void)
+
+{
+char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+{
+	_putchar(letter);
+}
+{
+	_putchar('\n');
+}
+}
