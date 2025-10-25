a6fabdb295fb469d1a819abdbee0fecaf78ac29b
AlexLucM <ouattaradounya@gmail.com>
2025-10-25 21:40:53 +0000

    added file 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_alphabet - prints alphabet in lowwer case x10, followed by a nex line
+*
+* Return: void
+*/
+void print_alphabet_x10(void)
+
+{
+int n;
+char letter;
+n = 0;
+while (n < 10)
+
+{
+	letter = 'a';
+	while (letter <= 'z')
+{
+	_putchar(letter);
+	letter++;
+}
+	_putchar('\n');
+	n++;
+}
+}
