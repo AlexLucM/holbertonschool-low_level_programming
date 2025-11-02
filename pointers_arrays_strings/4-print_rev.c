ec38eec4b03450b9659eee381334a192c116fb6c
AlexLucM <ouattaradounya@gmail.com>
2025-11-02 12:46:16 +0000

    added file 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_rev - prints a string in reverse
+*
+* @s : character to check
+*
+* Return: void
+*/
+void print_rev(char *s)
+
+{
+int i = 0;
+	while (s[i] != '\0')
+	i++;
+
+	i--;
+while (i >= 0)
+{
+	_putchar(s[i]);
+	i--;
+}
+
+	_putchar('\n');
+}
