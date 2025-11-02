a128c06aaee12427d7f3d558b5e7fc5e01d6f2fe
AlexLucM <ouattaradounya@gmail.com>
2025-11-02 13:24:32 +0000

    added file 5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* rev_string - reverses a string
+*
+* @s - charachter to reverse
+*
+* Return void
+*/
+void rev_string(char *s)
+
+{
+int i = 0;
+int len = 0;
+char rev;
+
+	while (s[len] != '\0')
+	len++;
+
+	while (i < len / 2)
+{
+	rev = s[i];
+	s[i] = s[len - 1 - i];
+	s[len - 1 - i] = rev;
+	i++;
+}
+}
+
