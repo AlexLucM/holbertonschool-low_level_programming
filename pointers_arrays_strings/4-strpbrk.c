43081b953d7d0ea06b55b9147aba7ff0d00c47b7
AlexLucM <ouattaradounya@gmail.com>
2025-11-13 00:15:12 +0000

    added file 4-strpbrk

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _strpbrk - locate the first occurence in the string s
+*  of any bytes in the string accept
+*
+* @s : string to check
+*
+* @accept : string to match
+*
+* Return: a pointer to the bytes in s that matches one of
+* the bytes in accept, or NULL if no such byte is found
+*/
+char *_strpbrk(char *s, char *accept)
+
+{
+int i = 0;
+int j;
+
+	while (s[i] != '\0')
+{
+	j = 0;
+
+	while (accept[j] != '\0')
+{
+	if (s[i] == accept[j])
+{
+	return (&s[i]);
+}
+	j++;
+}
+	i++;
+}
+	return (NULL);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -35,5 +35,6 @@ char *leet(char *);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
 
 #endif
