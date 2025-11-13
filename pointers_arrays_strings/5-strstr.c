599851e2248b05cc43515b2262738d462312cac9
AlexLucM <ouattaradounya@gmail.com>
2025-11-13 02:30:00 +0000

    fixed code

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -17,7 +17,6 @@ char *_strstr(char *haystack, char *needle)
 {
 int i = 0;
 int j;
-char *og = needle;
 
 	while (haystack[i] != '\0')
 {
@@ -25,12 +24,18 @@ char *og = needle;
 
 	while (needle[j] != '\0')
 {
-	if (haystack[i] == needle[j])
+	if (haystack[i + j] == needle[j])
 {
-	return (og);
-}
 	j++;
 }
+	else
+{
+	break;
+}
+}
+	if (needle[j] == '\0')
+	return (&haystack[i]);
+
 	i++;
 }
 	return (NULL);
