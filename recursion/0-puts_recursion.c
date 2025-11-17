db441769da8a7087495abab3393ff9b1431794b2
AlexLucM <ouattaradounya@gmail.com>
2025-11-17 15:53:26 +0000

    added file 0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _puts_recursion - prints a string folloxed by a new line
+*
+* @s : string to print
+*
+* Return: void
+*/
+void _puts_recursion(char *s)
+
+{
+	_putchar('s');
+}
